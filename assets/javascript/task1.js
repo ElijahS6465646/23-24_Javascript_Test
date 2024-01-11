34d62cf6bacb0fb98c480a26ef6adf7ec4d1f12f
Elijah S <elijah.suarez@vtsd.com>
2024-01-11 11:45:59 -0500

    Not submitted

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,1 +1,15 @@
 /* Put code for the task below */
+const button = document.query('button');
+const img = document.query('img');
+let display;
+function buttonHovered() {
+    if (buttonHovered) {
+        button.style.display = "block";
+    }
+
+}
+function buttonClicked() {
+    if(buttonClicked) {
+        img.style.display = "none";
+    }
+}
--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,1 +1,22 @@
 /* Put code for the task below */
+const rbutton = document.query('rbutton');
+const gbutton = document.query('gbutton');
+const bbutton = document.query('bbutton');
+const table = document.querySelector('table');
+function changeColor() {
+    if (function changeRed(){
+        table.style.color = "red";
+    }) {
+        rbutton.style.color = "red";
+    }
+    if (function changeBlue(){
+        table.style.color = "blue";
+    }) {
+        gbutton.style.color = "blue";
+    }
+    if (function changeGreen(){
+        table.style.color = "green";
+    }) {
+        bbutton.style.color = "green";
+    }
+}
